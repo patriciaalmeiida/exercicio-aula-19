0a87fa72ed67bfc908559249efc494b34b028693
patriciaalmeiida <paty_almeida@live.com>
2021-02-02 21:22:34 -0300

    exercicio-19-atualizado

--- aula-19/src/App.tsx
+++ aula-19/src/App.tsx
@@ -6,17 +6,24 @@ import axios from "axios";
 function App() {
   let inputNome = useRef<HTMLInputElement>(null);
   let inputIdade = useRef<HTMLInputElement>(null);
+  let inputEmpresa = useRef<HTMLInputElement>(null);
+  let inputTelefone = useRef<HTMLInputElement>(null);
 
   const [cadastou, setCadastrou] = useState<String>("");
 
   const enviarFormulario = () => {
     const nome = inputNome.current?.value;
     const idade = inputIdade.current?.value;
+    const empresa = inputEmpresa.current?.value;
+    const telefone = inputTelefone.current?.value;
 
     axios
       .post("http://localhost:4000/usuarios", {
         name: nome,
         age: idade,
+        company: empresa,
+        phone: telefone
+
       })
       .then((resposta) => {
         if (resposta.status === 201) {
@@ -29,6 +36,8 @@ function App() {
     <div className="App">
       <input type="text" placeholder="Digite seu nome" ref={inputNome} />
       <input type="text" placeholder="Digite sua idade" ref={inputIdade} />
+      <input type="text" placeholder="Digite sua empresa" ref={inputEmpresa} />
+      <input type="text" placeholder="Digite seu telefone" ref={inputTelefone} />
       <button onClick={enviarFormulario}>Enviar</button>
 
       <p></p>
